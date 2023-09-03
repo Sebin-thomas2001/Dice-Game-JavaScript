2447e09325b3cf1e3d31d2fb4a62974f640b2564
Sebin-thomas2001 <120407128+Sebin-thomas2001@users.noreply.github.com>
2023-09-03 23:13:34 +0530

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,33 @@
+
+var randomNumber1=Math.floor(Math.random()*6)+1; // 1-6
+
+var randomDiceImage="dice" + randomNumber1 +".png"; //dice1.png-dice6.png
+
+var randomImageSource="images/" + randomDiceImage; //images/sice1.png-dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImageSource2="images/dice"+ randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+//if statements//
+
+if(randomNumber1 >  randomNumber2){
+
+    document.querySelector("h1").innerHTML="😍 Player One Wins 😍";
+
+}
+
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="😍 Player Two Wins 😍";
+}
+
+else{
+    document.querySelector("h1").innerHTML="🥴Draw!🥴";
+}
